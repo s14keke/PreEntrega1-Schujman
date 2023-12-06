7df776c0773b666d056e4ac11d015e483d994494
Jeremias <schujmanjeremias05@gmail.com>
2023-12-06 13:57:42 -0300

    tercer commit con ciclo

--- js/index.js
+++ js/index.js
@@ -1,32 +1,57 @@
 function obtenerNumeroInput(mensaje) {
-    let input = prompt(mensaje);
-    let numero = parseFloat(input);
-    
-    if (isNaN(numero)) {
-        alert("Por favor, ingrese un número válido.");
-        return obtenerNumeroInput(mensaje);
-    }
-    
-    return numero;
+  let input = prompt(mensaje);
+  let numero = parseFloat(input);
+
+  if (isNaN(numero)) {
+      alert("Por favor, ingrese un número válido.");
+      return obtenerNumeroInput(mensaje);  
+  }
+
+  return numero;
 }
 
 function calcularPrecioFinal(precioBase, porcentajeImpuestos, porcentajeDescuento) {
-    let impuestos = (precioBase * porcentajeImpuestos) / 100;
-    let descuentos = (precioBase * porcentajeDescuento) / 100;
-    let precioFinal = precioBase + impuestos - descuentos;
-    
-    return {
-        precioBase: precioBase.toFixed(2),
-        impuestos: impuestos.toFixed(2),
-        descuentos: descuentos.toFixed(2),
-        precioFinal: precioFinal.toFixed(2)
-    };
+  let impuestos = (precioBase * porcentajeImpuestos) / 100;
+  let descuentos = (precioBase * porcentajeDescuento) / 100;
+  let precioFinal = precioBase + impuestos - descuentos;
+
+  return {
+      precioBase: precioBase.toFixed(2),
+      impuestos: impuestos.toFixed(2),
+      descuentos: descuentos.toFixed(2),
+      precioFinal: precioFinal.toFixed(2)
+  };
 }
 
-let precioBase = obtenerNumeroInput("Ingrese el precio base del producto:");let porcentajeImpuestos = obtenerNumeroInput("Ingrese el porcentaje de impuestos:");let porcentajeDescuento = obtenerNumeroInput("Ingrese el porcentaje de descuento:");
+let precioBase, porcentajeImpuestos, porcentajeDescuento;
+
+while (true) {
+  precioBase = obtenerNumeroInput("Ingrese el precio base del producto:");
+
+  if (!isNaN(precioBase)) {
+      break;
+  }
+}
+
+while (true) {
+  porcentajeImpuestos = obtenerNumeroInput("Ingrese el porcentaje de impuestos:");
+
+  if (!isNaN(porcentajeImpuestos)) {
+      break;
+  }
+}
+
+while (true) {
+  porcentajeDescuento = obtenerNumeroInput("Ingrese el porcentaje de descuento:");
+
+  if (!isNaN(porcentajeDescuento)) {
+      break;
+  }
+}
+
 let resultado = calcularPrecioFinal(precioBase, porcentajeImpuestos, porcentajeDescuento);
 
 alert("Precio base: $" + resultado.precioBase +
-      "\nImpuestos: $" + resultado.impuestos +
-      "\nDescuentos: $" + resultado.descuentos +
-      "\nPrecio final: $" + resultado.precioFinal);
+  "\nImpuestos: $" + resultado.impuestos +
+  "\nDescuentos: $" + resultado.descuentos +
+  "\nPrecio final: $" + resultado.precioFinal);
